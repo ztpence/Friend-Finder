0ff7160412bf39ffc226c994d4d817e275b304c9
Zane P <ztpence@gmail.com>
2018-05-01 16:19:42 -0700

    freinds added

---
+++ app/data/friends.js
@@ -0,0 +1,13 @@
+module.exports = [
+
+    {
+        name: 'Kurt',
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSyuvhXluxbeE28ZW1VPPMNIaisNbBGTHn7HUdP_Flqef_B8PN",
+        scores: [5,2,4,3,5,3,1,4,4,5]
+    },
+    {
+        name: 'Simone',
+        photo:"https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Ftimedotcom.files.wordpress.com%2F2016%2F08%2Fsimone-biles-simone-manuel-baby-names.jpg&w=700&q=85",
+        scores: [3,4,2,4,2,4,3,1,2,4]
+    }
+];
